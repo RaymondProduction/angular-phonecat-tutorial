712da02f3612cb8b81537e51929bb729f50d0056
Raymond Perminov <raymondproduction@ukr.net>
2018-01-19 02:23:35 +0200

    REST and Custom Services

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -5,18 +5,9 @@ angular.
   module('phoneList').
   component('phoneList', {
    templateUrl: 'phone-list/phone-list.template.html',
-    controller: ['$http', function PhoneListController($http) {
-      // Since we are making the assignment of the
-      // phones property in a callback function,
-      // where the this value is not defined,
-      // we also introduce a local variable called
-      // self that points back to the controller instance.
-      var self = this;
-      self.orderProp = 'age';
-      $http.get('phones/phones.json').then(function(response){
-        //self.phones = response.data;
-        self.phones = response.data.slice(0, 5);
-      })
+    controller: ['Phone', function PhoneListController(Phone) {
+      this.phones = Phone.query();
+      this.orderProp = 'age';
     }]
 
   });
